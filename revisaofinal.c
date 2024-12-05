f633baa0f80dee56f0e6520219d3986b578e9134
lavinia <goncalveslavinia633@gmail.com>
2024-12-05 20:15:05 -0300

    exercicios

---
+++ revisaofinal.c
@@ -0,0 +1,38 @@
+// incluir bibliotecas:
+#include <stdio.h>
+#include <stdlib.h>
+// método de abertura
+main()
+{
+    // tipos de dados
+    int inteiros;
+    char strings;
+    float numeros_decimais;
+    double numeros_decimais_grandes;
+    char [50] strings_maiores;
+
+    //tipos bidimensionais
+    int vetor[5];
+
+    // tipo multimensional
+    int matriz[5][5];
+    Calculo_salario(matriz[5][5]);
+    if(salario > 5000.00){
+        desconta_imposto();
+    }
+    printf();//mostrar mensagem parao usuario
+    scanf();// receber o valor via teclado
+    while(i<10){} // laço Enquanto a condicao for verdadeira faça.
+    // enquanto i for menor que 10, os comandos dentro das chaves serao executados
+    for(int i=0; i<10. i++){}
+    // para i = 0 ate i < 10 faça. O i++ serve para incrementar
+    switch /*constante*/
+}
+
+
+float Calculo_salario()
+{
+}
+funcao2()
+{
+}
